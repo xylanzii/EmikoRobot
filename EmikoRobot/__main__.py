e57491f11c321109988b5193c7820336c789f08b
xylanzii <102651517+xylanzii@users.noreply.github.com>
2022-04-05 13:04:04 +0700

    Update __main__.py

--- EmikoRobot/__main__.py
+++ EmikoRobot/__main__.py
@@ -428,7 +428,7 @@ def emiko_about_callback(update, context):
             text=f"<b>๏ Menyiapkan catatan</b>"
             f"\nAnda dapat menyimpan pesan/media/audio atau apa pun sebagai catatan"
             f"\untuk mendapatkan catatan cukup gunakan # di awal kata"
-            f"\n\Anda juga dapat mengatur tombol untuk catatan dan filter (lihat menu bantuan)",
+            f"\n\nAnda juga dapat mengatur tombol untuk catatan dan filter (lihat menu bantuan)",
             parse_mode=ParseMode.HTML,
             reply_markup=InlineKeyboardMarkup(
                 [[InlineKeyboardButton(text="Go Back", callback_data="emiko_")]]
